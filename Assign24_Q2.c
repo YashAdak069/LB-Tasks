33063463fb3dfea4fde0dd69f9f4b0f0ff724a85
YashAdak069 <adakyash31@gmail.com>
2026-06-15 16:00:33 +0530

    Pattern Printing Programs

---
+++ Assign24_Q2.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j = 0;
+    char ch = '\0';
+    char c = '\0';
+
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = 1, ch = 'A', c = 'a'; j <= iCol; j++, ch++, c++)
+        {
+           if((i % 2) != 0)
+           {
+            printf("%c\t",ch);
+           }
+           else
+           {
+            printf("%c\t",c);
+           }
+        }
+        printf("\n");        
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter the number of rows and colomns : ");
+    
+    printf("\n");
+    
+    printf("Rows : ");
+    scanf("%d",&iValue1);
+
+    printf("Colomns : ");
+    scanf("%d",&iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
