dabb353b59913761c3b2604ff107ea027bc7a3cf
Yash Adak <adakyash31@gmail.com>
2026-06-25 07:24:42 -0700

    String Reverse Operation

---
+++ Assign33-Q5.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+void StrRevX(char *str)
+{
+    char *start = NULL;
+    char *end = NULL;
+    char temp = '\0';
+
+    start = str;
+
+    while(*str != '\0')
+    {
+        str++;
+    }
+    
+    str--;
+    end = str;
+    
+   while(start < end)
+   {
+        temp = *start;
+        *start = *end;
+        *end = temp;
+
+        start++;
+        end--;
+   }
+}
+
+int main()
+{
+    char arr[20];
+
+    printf("Enyter string : \n");
+    scanf("%[^'\n']s",arr);
+
+    StrRevX(arr);
+
+    printf("MOdified string : %s\n",arr);
+    
+    return 0;
+}
